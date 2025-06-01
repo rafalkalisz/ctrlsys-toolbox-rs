def5955bddad735cbd9581f6a2ab4fe1d5737396
Rafal Kalisz <rafal.kalisz05@gmail.com>
2025-06-01 14:08:08 +0200

    Improve pole-zero plot marker visibility

--- src/plot/pz.rs
+++ src/plot/pz.rs
@@ -9,6 +9,9 @@ pub fn pzplot(ui: &mut egui::Ui, tf: &dyn TransferFunction) {
     let poles = tf.poles();
     let zeroes = tf.zeroes();
 
+    let pole_coords: Vec<[f64; 2]> = poles.iter().map(|complex| [complex.re, complex.im]).collect();
+    let zero_coords: Vec<[f64; 2]> = zeroes.iter().map(|complex| [complex.re, complex.im]).collect();
+
     let all_points = poles.iter().chain(zeroes.iter());
 
     let (min_re, max_re, min_im, max_im) = all_points.fold(
@@ -23,6 +26,16 @@ pub fn pzplot(ui: &mut egui::Ui, tf: &dyn TransferFunction) {
         }
     );
 
+    let pole_points = egui_plot::Points::new("poles", pole_coords)
+        .color(egui::Color32::MAGENTA)
+        .radius(6.0)
+        .shape(egui_plot::MarkerShape::Cross);
+
+    let zero_points = egui_plot::Points::new("poles", zero_coords)
+        .color(egui::Color32::CYAN)
+        .radius(6.0)
+        .shape(egui_plot::MarkerShape::Circle);
+
     let margin = 0.5;
     let x_min = min_re - margin;
     let x_max = (max_re + margin).max(margin); // Make sure the Y axis is visible
@@ -35,17 +48,7 @@ pub fn pzplot(ui: &mut egui::Ui, tf: &dyn TransferFunction) {
         .include_y(y_min)
         .include_y(y_max)
         .show(ui, |plot_ui| {
-
-            for pole in poles {
-                plot_ui.text(Text::new("pole", PlotPoint::new(pole.re, pole.im), "X")
-                    .color(egui::Color32::MAGENTA)
-                );      
-            }
-            for zero in zeroes {
-                plot_ui.text(Text::new("zero", PlotPoint::new(zero.re, zero.im), "O")
-                    .color(egui::Color32::CYAN)
-                );
-            }
-
+            plot_ui.points(pole_points);
+            plot_ui.points(zero_points);
         });
 }
